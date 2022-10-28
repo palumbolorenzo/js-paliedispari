1fd04c879f5e2a6cc2630df8195b81d8176dc790
palumbolorenzo <palumbolorenzo97@gmail.com>
2022-10-28 23:19:15 +0200

    provvisorio

--- js/script.js
+++ js/script.js
@@ -1,55 +1,72 @@
-
 /*
-function somma(num1, num2) {
-    return num1 + num2;
-}
-
-console.log(somma(5, 2));
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
 */
 
-
 /*
-for (let i = 0; i < 10; i++) {
-    console.log(getRandomInteger(1, 100));
+l'utente sceglie pari o dispari
+l'utente inserisce un numero da 1 a 5
+il computer genera una altro numero random da 1 a 5
+sommiamo e paragoniamo i numeri (funzione);
+*/
+
+const userOddOrEven = prompt('pari o dispari');
+const userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
+let computerNumber;
+
+let i = computerNumber;
+while (i < 5) {
+    console.log(getRandomInteger(1, 5));
+    i++
 }
 
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-*/
 
-const userNumber = parseInt(prompt('dammi un numero'));
-//tellUserDivisibility(userNumber);
+let sum = somma(userNumber + computerNumber);
 
-if (isEven(userNumber)) {
-    console.log('pari');
-} else {
-    console.log('dispari');
+function somma(a, b) {
+    if (somma % 2 == 0) {
+        console.log('il valore è pari');
+    } else {
+        console.log('il valore è dispari');
+    }
+    return a + b;
 }
+
+if (userOddOrEven === 'pari' && somma % 2 == 0) {
+    console.log('hai vinto');
+} else (console.log('hai perso'));
+
+if (userOddOrEven === 'dispari' && somma % 2 != 0) {
+    console.log('hai vinto');
+} else (console.log('hai perso'));
 
 /*
-if (userNumber % 2 === 0) {
-    console.log('pari');
-} else {
-    console.log('dispari');
-}
+Palindroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
 */
 
+const userWord = prompt('inserisci una parola');
+let palindrome = controlloPalindromo(userWord);
+console.log(palindrome);
 
-function tellUserDivisibility (number) {
-    console.log(number);
-    if (number % 2) {
-        console.log('dispari');
-    } else {
-        console.log('pari');
-    }
-}
+let parola = controlloPalindromo;
 
+function controlloPalindromo(parola) {
+    const lunghezza = parola.length;
 
-function isEven(number) {
-    if (number % 2) {
-        return false;
-    } else {
-        return true;
-    }
-}
+    for (let i = 0; i < lunghezza / 2; i++) {
+        if (parola[i] != parola[lunghezza - 1 - i]) {
+            return 'la parola non è palindroma';
+        } else {
+            return 'la parola è palindroma';
+        }
+    } 
+};
